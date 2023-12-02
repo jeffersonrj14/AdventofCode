e7442a976efc1ca33a2db71505c028a292d51c4b
Jefferson <stupidjeff430@gmail.com>
2023-12-03 01:19:35 +0900

    update-c

--- day-1/solution.c
+++ day-1/solution.c
@@ -1,4 +1,3 @@
-// It took me 4 hours to understand because I lack problem-solving skills, that's why I joined Advent of Code.
 // Started late because solving one question might take two days.
 #include <stdio.h>
 #include <stdlib.h>
